309b2b61da7517db7e271cb3ae0cd3b4a90343a3
PremCNX <95966304+PremCNX@users.noreply.github.com>
2021-12-12 16:24:40 +0700

    First upload package file

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# PremCNXprofile
-ตัวอย่างการอัพโหลดไฟล์ขึ้น PyPI
+# (PremCNX Profile) เป็นตัวอย่างการอัพโหลด Packageไปยัง pypi.org
+
+PyPi: https://pypi.org/project/PremCNXmyprofile/
+
+แพคเก็จสำหรับอธิบาย การอัพไฟล์ไปยัง pypi.org
+### วิธีติดตั้ง
+
+เปิด CMD / Terminal
+
+```python
+pip install PremCNXmyprofile
+```
+### วิธีใช้งานแพ็คเพจนี้
+Example
+	my = Profile('Premchai')
+	my.company = 'hana'
+	my.hobby = ['FX Trader','Reading','Youtube']
+	print(my.name)
+	my.show_email()
+	my.show_myart()
+	my.show_hobby()
+
+พัฒนาโดย: 
+PremCNX
---
+++ setup.py
@@ -0,0 +1,31 @@
+﻿import io
+from os.path import abspath, dirname, join
+from setuptools import find_packages, setup
+
+
+HERE = dirname(abspath(__file__))
+LOAD_TEXT = lambda name: io.open(join(HERE, name), encoding='UTF-8').read()
+DESCRIPTION = '\n\n'.join(LOAD_TEXT(_) for _ in [
+    'README.rst'
+])
+
+setup(
+  name = 'PremCNXprofile',      
+  packages = ['PremCNX'], 
+  version = '0.0.1',  
+  license='MIT', 
+  description = 'PremCNX Profile',
+  long_description=DESCRIPTION,
+  author = 'PremCNX',                 
+  author_email = 'kt.premchai@gmail.com',     
+  url = 'https://github.com/PremCNX/PremCNXmyprofile',  
+  download_url = 'https://github.com/PremCNX/PremCNX/archive/v0.0.1.zip',  
+  keywords = ['PremCNX'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Education',     
+    'Topic :: Software Development :: Utilities',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3.10',
+  ],
+)
